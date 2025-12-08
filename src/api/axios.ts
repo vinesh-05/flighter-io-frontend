d546f59d3cf36c0d30a6ac6fa046eb35b0923624
vinesh-05 <2005vinesh@gmail.com>
2025-12-08 16:34:37 +0530

    proper base url uploaded

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "flighter-io-production.up.railway.app", // your FastAPI URL
+  baseURL: "https://flighter-io-production.up.railway.app", // your FastAPI URL
 });
 
 // Attach JWT in every request
